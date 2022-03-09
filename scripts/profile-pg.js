cca038f80267a50ec657b38bc73e1ba64452950a
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-03-10 01:01:57 +0530

    style - create a post modal added

--- scripts/profile-pg.js
+++ scripts/profile-pg.js
@@ -1,5 +1,5 @@
 const openDialogBtn = document.querySelector('.open-modal')
-const modalDiv = document.querySelector('.modal')
+// const modalDiv = document.querySelector('.modal')
 const modalContainer = document.querySelector('.modal-container')
 const dialogActionBtn = document.querySelectorAll('.dialog-action-btn')
 
@@ -16,5 +16,26 @@ dialogActionBtn.forEach(actionBtn => {
         modalContainer.classList.remove('pos-fixed')
         modalContainer.classList.remove('tl-0')
         modalContainer.classList.remove('z-5')
+    })
+})
+
+const openDialogBtn2 = document.querySelector('#btn-post')
+// const modalDiv2 = document.querySelector('.modal2')
+const modalContainer2 = document.querySelector('.modal2-container')
+const dialogActionBtn2 = document.querySelectorAll('.dialog2-action-btn')
+
+openDialogBtn2.addEventListener('click', () => {
+    modalContainer2.classList.remove('dis-hidden')
+    modalContainer2.classList.add('pos-fixed')
+    modalContainer2.classList.add('tl-0')
+    modalContainer2.classList.add('z-5')
+})
+
+dialogActionBtn2.forEach(actionBtn => {
+    actionBtn.addEventListener('click', () => {
+        modalContainer2.classList.add('dis-hidden')
+        modalContainer2.classList.remove('pos-fixed')
+        modalContainer2.classList.remove('tl-0')
+        modalContainer2.classList.remove('z-5')
     })
 })
